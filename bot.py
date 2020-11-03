9e4191caa65cf5bfe4762022aab35f35418a46cc
mastermuna <53406112+mastermuna@users.noreply.github.com>
2020-11-03 14:20:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,4 +111,4 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=discord.Game('teke apua'))
 
 
-client.run('769280223798755338')
+client.run('NzY5MjgwMjIzNzk4NzU1MzM4.X5MuCQ.01sKEoZq3gPRN2GVRUS4Tjv_TVo')
